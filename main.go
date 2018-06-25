794b3730e2814a4ccdaba98c14719a5452619b31
Eugene <gish.ee@gmail.com>
2018-06-25 19:13:39 +0300

    Add prefix support, add todos

--- main.go
+++ main.go
@@ -9,15 +9,20 @@ import (
 	"strconv"
 	"strings"
 	"syscall"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
+var (
+	prefixes []string
+)
+
 type Config struct {
-	Token string `json: "token"`
+	Token  string `json: "token"`
+	Prefix string `json: "prefix"`
 }
 
+// TODO: move to separate file
 func readConfig() (Config, error) {
 	var c Config
 
@@ -40,19 +45,21 @@ func main() {
 
 	dg, err := discordgo.New("Bot " + config.Token)
 	if err != nil {
-		fmt.Println("Error creating Discord session,", err)
+		fmt.Println("Fatal: creating Discord session,", err)
 		return
 	}
-
-	dg.AddHandler(messageCreate)
 
 	err = dg.Open()
 	if err != nil {
-		fmt.Println("Error opening connection,", err)
+		fmt.Println("Fatal: opening connection,", err)
 		return
 	}
 
-	fmt.Println("Bot is ready")
+	prefixes = append(prefixes, config.Prefix, "<@"+dg.State.User.ID+">", "<@!"+dg.State.User.ID+">")
+
+	dg.AddHandler(messageCreate)
+
+	fmt.Println("Info: Bot is ready") // TODO: logger
 
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
@@ -61,12 +68,48 @@ func main() {
 	dg.Close()
 }
 
+func checkPrefixes(content string) string {
+	// TODO: guild prefix override
+
+	lowerContent := strings.ToLower(content)
+
+	for _, p := range prefixes {
+		if strings.HasPrefix(lowerContent, p) {
+			return p
+		}
+	}
+
+	return ""
+}
+
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.Bot {
 		return
 	}
 
-	switch strings.ToLower(m.Content) {
+	prefix := checkPrefixes(m.Content)
+
+	if prefix == "" {
+		channel, err := s.State.Channel(m.ChannelID)
+		if err != nil {
+			fmt.Println("Channel with id %d not found", m.ChannelID)
+			return
+		}
+		if channel.Type != discordgo.ChannelTypeDM { // empty prefix is  allowed for direct messages
+			return
+		}
+	}
+
+	// TODO: blacklist guild/user/(channel?)
+
+	// TODO: argument parser, flag parser
+	args := strings.Fields(m.Content[len(prefix):])
+
+	// TODO: command handler
+	// TODO: response registration using redis
+	switch strings.ToLower(strings.ToLower(args[0])) {
+	case "help":
+		s.ChannelMessageSend(m.ChannelID, "help, ping, uptime, user")
 	case "ping":
 		pingMessage, err := s.ChannelMessageSend(m.ChannelID, "Pinging...")
 		if err != nil {
@@ -83,11 +126,14 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			return
 		}
 
-		delta := (id2 - id1) >> 22
+		delta := id2>>22 - id1>>22 // TODO: use timestamps
 
-		s.ChannelMessageEdit(m.ChannelID, pingMessage.ID, fmt.Sprintf("Pong, it took **%d**ms to respond", delta))
-	case "sleep":
-		time.Sleep(5 * time.Second)
-		s.ChannelMessageSend(m.ChannelID, "Done sleeping")
+		s.ChannelMessageEdit(m.ChannelID, pingMessage.ID, fmt.Sprintf("Pong, it took **%dms** to respond", delta))
+	case "uptime":
+		s.ChannelMessageSend(m.ChannelID, "Todo")
+	case "user":
+		// TODO: getUser
+		// TODO: other get functions (utilize?)
+		s.ChannelMessageSend(m.ChannelID, "Todo")
 	}
 }
